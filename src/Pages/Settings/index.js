1a889ea139534fc1cd3a5406ad69ce648110d88c
Guilherme Viana <guilhermehviana01@gmail.com>
2021-11-29 13:46:08 -0300

    Add settings initial style

--- src/Pages/Settings/index.js
+++ src/Pages/Settings/index.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import './Settings.css';
 
 export default class Settings extends Component {
   render() {
     return (
-      <div>
+      <section className="settings-section">
         <h1 data-testid="settings-title">Settings</h1>
-      </div>
+      </section>
     );
   }
 }
